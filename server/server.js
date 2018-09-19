9f1776ace091551d5c46937fa81aa7168c4ffbf6
Hariom Mukati <hariommukati1995@gmail.com>
2018-09-19 14:28:12 +0530

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,6 +15,30 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user is connected');
 
+
+    //socket.emit from admin text welcome to the chat app
+    // socket.on('newCreateMessage', (message) => {
+    //     console.log('newCreateMessage', message);
+    //     io.emit('newMessage',{
+    //         from: message.from,
+    //         text: message.text,
+    //         createAt: new Date().getTime()
+    //     });
+    // });
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'this is from Admin message',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage',{
+       from: 'New User',
+       text: 'new user are connected',
+       createdAt: new Date().getTime()
+    });
+    //socket.broadcast.emit from admin text New user join
+
+
     socket.on('createMessage', (message)=>{
        console.log('createMessage', message);
        io.emit('newMessage', {
@@ -22,6 +46,11 @@ io.on('connection', (socket) => {
            text: message.text,
            createdAt: new Date().getTime()
        });
+    //     socket.broadcast.emit('newMessage',{
+    //         from: message.from,
+    //         text: message.text,
+    //         createAt: new Date().getTime()
+    //     });
     });
 
     socket.on('disconnect', () => {
